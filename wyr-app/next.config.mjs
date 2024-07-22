001bb496bc79410983c4973b40f3eebf5f601152
Kshitij Kochhar <kshitij@ctfguide.com>
2024-07-22 15:41:57 -0400

    [fix] updated config file

--- wyr-app/next.config.mjs
+++ wyr-app/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default {
+    basePath: "/Would_You_Rather_App",
+    assetPrefix: "/Would_You_Rather_App/",
+};
